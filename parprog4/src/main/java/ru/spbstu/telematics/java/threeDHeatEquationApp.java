9b8de204a3d294e6d002ba955ee08890b330c22a
foria1405 <fominadd1405@yandex.ru>
2021-02-15 12:00:34 +0300

    parprog4

---
+++ parprog4/src/main/java/ru/spbstu/telematics/java/threeDHeatEquationApp.java
@@ -0,0 +1,32 @@
+package ru.spbstu.telematics.java;
+
+import java.time.Duration;
+import java.time.Instant;
+
+
+public class threeDHeatEquationApp {
+
+    public static void main(String[] args) throws InterruptedException {
+        int temp = 22;
+        int iterations = 40000;
+        int nodes = 50;
+        int maxParallelism = 7;
+
+
+        double[][][] TInit = MatrixGenerator.getRandomMatrix(temp, nodes, nodes, nodes);
+
+            for (int i = 1; i < maxParallelism; i++) {
+                HeatEquationSolver heatEquationSolver = new HeatEquationSolver(i);
+                ConsoleMatrixOutput consoleMatrixOutput = new ConsoleMatrixOutput();
+                System.out.println("Number of nodes:" + nodes);
+                System.out.println("Number of threads:" + i);
+                Instant start = Instant.now();
+                heatEquationSolver.calculate(TInit, nodes, nodes, nodes, iterations, temp);
+                Instant finish = Instant.now();
+                System.out.printf("Working time = %d milliseconds%n", Duration.between(start, finish).toMillis());
+            }
+       // }
+    }
+
+
+}
